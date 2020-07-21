35d55f3e36821c6f081330e7c54b24e5a2f8645a
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2020-07-21 10:58:18 +0530

    Add SafeMath library

--- contracts/LiquidationManager.sol
+++ contracts/LiquidationManager.sol
@@ -3,6 +3,7 @@ pragma solidity 0.5.12;
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/ownership/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
@@ -11,7 +12,7 @@ import "./interfaces/ICToken.sol";
 
 contract LiquidityManagement {
     using SafeERC20 for IERC20;
-
+    using SafeMath for uint256;
 
     address public comptrollerAddress = address(
         0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B
