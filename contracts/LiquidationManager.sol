d3f4635e2609e10a35bad3260d7933d8c3a4b1b3
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2020-07-21 10:58:01 +0530

    Add SafeERC20 library

--- contracts/LiquidationManager.sol
+++ contracts/LiquidationManager.sol
@@ -1,9 +1,18 @@
 pragma solidity 0.5.12;
 
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
 import "./interfaces/IComptroller.sol";
 import "./interfaces/ICToken.sol";
 
 contract LiquidityManagement {
+    using SafeERC20 for IERC20;
+
+
     address public comptrollerAddress = address(
         0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B
     );
