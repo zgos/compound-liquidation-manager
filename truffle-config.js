6c8eb62bdc005f37714b6b2c416b7a1993442979
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2020-07-21 10:53:15 +0530

    Update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -5,8 +5,34 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
-    }
-  }
+    development: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+    },
+    coverage: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+    },
+  },
+  plugins: ["solidity-coverage"],
+  mocha: {
+    reporter: "eth-gas-reporter",
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.5.12", // Fetch exact version from solc-bin (default: truffle's version)
+      settings: {
+        // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 500,
+        },
+      },
+      evmVersion: "petersburg",
+    },
+  },
 };
