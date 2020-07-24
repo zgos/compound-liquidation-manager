bfe671cd27adc528eb81d6546d252d5e1878ee46
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2020-07-24 19:44:01 +0530

    Added menuitem component

---
+++ client/src/component/menuItem/index.js
@@ -0,0 +1,113 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import { makeStyles } from "@material-ui/core/styles";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import { tokenAddress } from "../../constant";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    marginLeft: theme.spacing(2),
+  },
+  paper: {
+    marginRight: theme.spacing(2),
+    zIndex: 9999
+  },
+}));
+
+const MenuListComposition = (props) => {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event, value) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+    if (value) {
+      props.handleSelected(value);
+    }
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
+
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+  return (
+    <div className={classes.root}>
+      <Button
+        ref={anchorRef}
+        aria-controls={open ? "menu-list-grow" : undefined}
+        aria-haspopup="true"
+        onClick={handleToggle}
+      >
+        {props.selected ? props.selected : "Select Token"}
+        <ArrowDropDownIcon />
+      </Button>
+      <Popper
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+        style={{ zIndex: 999 }}
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === "bottom" ? "center top" : "center bottom",
+            }}
+          >
+            <Paper elevation={3}>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList
+                  autoFocusItem={open}
+                  id="menu-list-grow"
+                  onKeyDown={handleListKeyDown}
+                >
+                  {Object.keys(tokenAddress).map((element, index) => {
+                    return (
+                      <MenuItem
+                        key={index}
+                        onClick={(event) => handleClose(event, element)}
+                      >
+                        {element}
+                      </MenuItem>)
+                  }
+                  )}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </div>
+  );
+}
+
+export default MenuListComposition;
