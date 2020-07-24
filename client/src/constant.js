a198b3ad0dd0c8e150ab891d1db291a9e8a7816a
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2020-07-24 19:31:21 +0530

    Add constant file

---
+++ client/src/constant.js
@@ -0,0 +1,8 @@
+export const tokenAddress = {
+  DAI: ['0x4f96fe3b7a6cf9725f59d353f723c1bdb64ca6aa', 18],
+  USDC: ['0xb7a4f3e9097c08da09517b5ab877f7a917224ede', 6],
+  WBTC: ['0xd3a691c852cdb01e281545a27064741f0b7f6825', 8],
+  ETH: ['0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE', 18]
+};
+
+export const contractAddress = '0xE7d072e5C298BFc3659C20E193dD6AAB1a716748';
